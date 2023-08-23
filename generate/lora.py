a721cac44e26a4ddeba3c944c3dad6a80119992d
Oscar Pindaro <oscar.pindaro@intellico.ai>
2023-08-23 07:46:20 +0000

    changed parameters from generation file to match finetuned model

--- generate/lora.py
+++ generate/lora.py
@@ -23,11 +23,11 @@ lora_r = 8
 lora_alpha = 16
 lora_dropout = 0.05
 lora_query = True
-lora_key = False
+lora_key = True
 lora_value = True
-lora_projection = False
-lora_mlp = False
-lora_head = False
+lora_projection = True
+lora_mlp = True
+lora_head = True
 
 
 def main(
